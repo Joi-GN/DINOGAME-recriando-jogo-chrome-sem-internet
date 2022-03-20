3027a333cacaf3c32b70dcf6a21b1659a7dcc46f
Joi-GN <joicegomesdn@gmail.com>
2022-03-20 16:08:06 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# DINO GAME - Desafio Recriando o jogo do dinossauro sem internet
+Projeto desenvolvido para o desafio Recriando o famoso jogo do dinossauro sem internet.  
+
+Para o desenvolvimento do jogo foi utilizado HTML, CSS e JavaScript.  
+
+Com o JavaScript, também implementei a ferramenta de mudança da página para modo escuro ou claro.
+
+![](gif1.gif)
+
+O jogo pode ser acessado por um navegador de internet.
---
+++ assets/script.js
@@ -0,0 +1,114 @@
+//DINOGAME
+
+const body = document.getElementsByTagName('body')[0];
+const background = document.querySelector('.background');
+const start = document.querySelector('.start');
+let jumpsound = document.querySelector('.jumpsound');
+let position = 0;
+let jumping = false;
+
+
+function startGame() {
+
+    //create player
+    const player = document.createElement('div');
+    player.classList.add('character');
+    background.appendChild(player);
+    background.removeChild(start);
+
+    //dark-mode
+    if (background.classList.contains("darkmode-bg")) {
+        player.style.backgroundImage = "url(assets/imgs/dino-dark-mode.png)";
+    } else {
+        player.style.backgroundImage = "url(assets/imgs/dino.png)";
+    }
+
+    //Space key pressed => player jump
+    window.addEventListener('keyup', (event) => {
+        if (event.code === 'Space') {
+            if (!jumping) {
+                jump();
+                jumpsound.play();
+            }
+        }
+    });
+
+    function jump() {
+        jumping = true;
+
+        //validation - height limit 
+        let upInterval = setInterval(() => {
+            if (position >= 150) {
+                clearInterval(upInterval);
+
+                let downInterval = setInterval(() => {
+
+                    //validation - bottom limit
+                    if (position <= 0) {
+                        clearInterval(downInterval);
+                        jumping = false;
+
+                    } else {
+
+                        //falling
+                        position -= 20;
+                        player.style.bottom = `${position}px`;
+                    }
+                }, 20)
+
+            } else {
+
+                //jumping
+                position += 20;
+                player.style.bottom = `${position}px`;
+            }
+        }, 20);
+    }
+
+    //Obstacles
+    function createObstacles() {
+        const obstacle = document.createElement('div');
+        obstacle.classList.add('obstacle');
+        background.appendChild(obstacle);
+
+        let obstaclePosition = 0;
+        let randomTime = Math.random() * 6000;
+
+        //dark-mode
+        if (background.classList.contains("darkmode-bg")) {
+            obstacle.style.background = "url(assets/imgs/cactus-dark-mode.png)";
+        } else {
+            obstacle.style.background = "url(assets/imgs/cactus.png)";
+        }
+
+        //obstacle's movement
+        let leftInterval = setInterval(() => {
+
+            //Validation - Obstacle out of background
+            if (obstaclePosition === 900) {
+                clearInterval(leftInterval);
+                background.removeChild(obstacle);
+
+                //GameOver
+            } else if (obstaclePosition < 900 && obstaclePosition > 840 && position < 60) {
+                clearInterval(leftInterval);
+                clearTimeout(spawnObstacles);
+
+                background.innerHTML = '<h2> Game Over </h2><button onClick="window.location.reload();"><i class="fa-solid fa-arrow-rotate-left"></i></button>'
+
+            } else {
+                obstaclePosition += 10;
+                obstacle.style.right = `${obstaclePosition}px`
+            }
+        }, 20);
+
+        let spawnObstacles = setTimeout(createObstacles, randomTime);
+    }
+
+    createObstacles();
+
+}
+
+function changeMode() {
+    body.classList.toggle("darkmode");
+}
